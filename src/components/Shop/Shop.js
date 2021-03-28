0e98a420befe58afc9b063f5c4b74946af0478b4
iswoad <iswoadansary@gmail.com>
2021-03-29 01:31:03 +0600

    work on header component

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import fakeData from '../../fakeData';
+import './Shop.css'
+
+const Shop = () => {
+    console.log(fakeData)
+    return (
+        <div>
+            <h1>this is shop</h1>
+        </div>
+    );
+};
+
+export default Shop;
