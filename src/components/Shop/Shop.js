3e454b07e55f173a7b0c852f9cbaa3e6f7f56863
iswoad <iswoadansary@gmail.com>
2021-03-30 03:18:01 +0600

    added event handler in add to cart button

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,17 +2,25 @@ import React from 'react';
 import fakeData from '../../fakeData';
 import './Shop.css'
 import { useState } from 'react';
+import Product from '../Product/Product'
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [products, setProducts] = useState(first10);
+
+    const handleAddProduct = (product) =>{
+        console.log('added', product)
+    }
+
     return (
         <div className='shop-container'>
             <div className="product-container">
-                <h1>this is shop</h1>
-                <h3>{ products.length }</h3>
-                <ul>
-                    {products.map( product => <li> {product.name} </li>)}
+                <ul>    
+                    {products.map( pd => <Product 
+                    product = {pd}
+                    handleAddProduct = {handleAddProduct}
+                    >
+                    </Product>)}
                 </ul>
             </div>
 
