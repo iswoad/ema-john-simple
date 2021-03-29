6f89d7c2d62ceb08505a1f2d2e8ffc4fbf0e2f9f
iswoad <iswoadansary@gmail.com>
2021-03-29 17:30:20 +0600

    structured shop-container

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import fakeData from '../../fakeData';
 import './Shop.css'
+import { useState } from 'react';
 
 const Shop = () => {
-    console.log(fakeData)
+    const first10 = fakeData.slice(0,10);
+    const [products, setProducts] = useState(first10);
     return (
-        <div>
-            <h1>this is shop</h1>
+        <div className='shop-container'>
+            <div className="product-container">
+                <h1>this is shop</h1>
+                <h3>{ products.length }</h3>
+                <ul>
+                    {products.map( product => <li> {product.name} </li>)}
+                </ul>
+            </div>
+
+            <div className="cart-container">
+                <h1>this is cart</h1>
+            </div>
+           
         </div>
     );
 };
