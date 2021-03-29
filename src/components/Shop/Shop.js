abb3d37a3eadd857027a586f89f2486e093b01ba
iswoad <iswoadansary@gmail.com>
2021-03-30 03:57:12 +0600

    added usestate to cart

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,9 +7,12 @@ import Product from '../Product/Product'
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([])
 
     const handleAddProduct = (product) =>{
         console.log('added', product)
+        const newCart = [...cart, product];
+        setCart(newCart)
     }
 
     return (
@@ -26,6 +29,7 @@ const Shop = () => {
 
             <div className="cart-container">
                 <h1>this is cart</h1>
+                <h5>Order Summary: {cart.length}</h5>
             </div>
            
         </div>
