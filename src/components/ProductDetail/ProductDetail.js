91a018c163058e85f2ca48d981e2d7869bc5cdeb
iswoad <iswoadansary@gmail.com>
2021-04-17 23:55:52 +0600

    console logged prodcuct

--- src/components/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useParams } from 'react-router';
+import fakeData from '../../fakeData';
 
 const ProductDetail = () => {
     const { productKey } = useParams();
+    const product = fakeData.find( pd => pd.key === productKey);
+    console.log(product);
     return (
         <div>
             <h1> { productKey } Product detail is comming sooooooonn......</h1>
