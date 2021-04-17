679c6773b7cbad93a4c494568582c7c0b64b016f
iswoad <iswoadansary@gmail.com>
2021-04-17 22:27:40 +0600

    used link element from react-router

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,17 +2,18 @@ import React from 'react';
 import './Product.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 
 const Product = (props) => {
-    const { img, name,seller,price,stock } = props.product;
+    const { img, name,seller,price,stock, key } = props.product;
     return (
         <div className="single-product">
             <div>
                 <img src={img} alt=""/>
             </div>
             <div>
-                <h4 className="product-name"> { name } </h4>
+                <h4 className="product-name"> <Link to = {"/product/"+key}> {name} </Link> </h4>
                 <br/>
                 <p>by:{seller}</p>
                 <br/>
