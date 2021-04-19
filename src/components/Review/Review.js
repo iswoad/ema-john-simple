443e1b083b7a178c45ac934e1424aaa53c8ba417
iswoad <iswoadansary@gmail.com>
2021-04-19 23:18:50 +0600

    removed item from database by handle remove product function

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { getDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, removeFromDatabaseCart } from '../../utilities/databaseManager';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Review = () => {
     const [cart, setCart] = useState([]);
 
     const removeItem = (productKey) => {
-        console.log('remove clicked', productKey);
         const newCart = cart.filter(pd => pd.key !== productKey);
         setCart(newCart);
+        removeFromDatabaseCart(productKey);
     }
 
     useEffect( () => {
