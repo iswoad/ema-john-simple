e9708a659a587746bed2013a7a29dde0a726aaf6
iswoad <iswoadansary@gmail.com>
2021-06-03 17:56:37 +0600

    added new comps, installed firebase, integrated ggl login

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDt2jl1mMrjAHNyVV7DZIaQfGJzSKqfTgM",
+    authDomain: "ema-john-simple-a17bf.firebaseapp.com",
+    projectId: "ema-john-simple-a17bf",
+    storageBucket: "ema-john-simple-a17bf.appspot.com",
+    messagingSenderId: "1034386467233",
+    appId: "1:1034386467233:web:827d0a3703477ae15403e0"
+  };
+export default firebaseConfig;
