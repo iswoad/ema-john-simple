fec04e40d7c99de0abdbac47ce190ad0ddd42a2b
iswoad <iswoadansary@gmail.com>
2021-06-04 01:44:46 +0600

    used private route for shipment comp via login comp

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ProductDetail from './components/ProductDetail/ProductDetail';
 import Shipment from './components/Shipment/Shipment';
 import Login from './components/Login/Login';
 import { createContext, useState } from 'react';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 export const UserContext = createContext();
 
@@ -33,12 +34,12 @@ function App(props) {
           <Route path ="/review">
             <Review></Review>
           </Route>
-          <Route path="/inventory">
+          <PrivateRoute path="/inventory">
             <Inventory></Inventory>
-          </Route>
-          <Route path="/shipment">
+          </PrivateRoute>
+          <PrivateRoute path="/shipment">
             <Shipment></Shipment>
-          </Route>
+          </PrivateRoute>
           <Route path="/login">
             <Login></Login>
           </Route>
