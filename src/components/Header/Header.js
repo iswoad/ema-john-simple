ae4943cf2aaa1f3ba8b282c190b7c371a59e57c8
iswoad <iswoadansary@gmail.com>
2021-06-04 21:04:13 +0600

    implemented signout & redirected from private route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../../App';
 import logo from '../../images/logo.png';
 import './Header.css';
 
-const header = () => {
+const Header = () => {
+
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
     return (
         <div class='header'>
            <img src={logo} alt=""/>
@@ -11,9 +15,10 @@ const header = () => {
                <Link to="/shop">Shop</Link>
                <Link to="/review">Order Review</Link>
                <Link to="/inventory">Manage Inventory</Link>
+               <button onClick = {() => setLoggedInUser({})}>Sign Out</button>
                </nav>
         </div>
     );
 };
 
-export default header;
+export default Header;
